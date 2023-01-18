20f0e612015eb8fc83236f81adffed1cddc9627a
acafonsosilva <ac.afonsosilva@gmail.com>
2023-01-18 00:53:03 +0000

    cleaning

---
+++ docs/.shiny_run/shinyShortcut.r
@@ -0,0 +1,3 @@
+#! /Library/Frameworks/R.framework/Resources/bin/Rscript
+
+shiny::runApp('/Users/acas/Dropbox/GitHub/R_tables_graphicExamples/docs', launch.browser = TRUE)
--- docs/app.R
+++ docs/app.R
@@ -148,5 +148,5 @@ server <- function(input, output, session){
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server, options = list(port=4184))
 
